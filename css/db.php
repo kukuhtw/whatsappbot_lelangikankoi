0c2719099ec95094e1108a046e19d7ea233f3c24
Kukuh TW <kukuhtw@gmail.com>
2023-04-13 09:43:35 +0700

    add folder
    add folder

---
+++ css/db.php
@@ -0,0 +1,18 @@
+﻿<?php
+//ini setting untuk dilocalhost , development
+
+$mySQLserver = "localhost";
+$mySQLuser = "phpmyadmin";
+$mySQLpassword = "M6rufuf%$^93mf90Ff823tg";
+$mySQLdefaultdb = "pharmamed";
+//$host = "139.59.234.51/";
+$host = "pharmamed.online/";
+$folderweb="";
+$webhook = $host."webhook/";
+
+
+$link = mysqli_connect($mySQLserver, $mySQLuser, $mySQLpassword,$mySQLdefaultdb) or die ("Could not connect to MySQL");
+
+
+
+?>
